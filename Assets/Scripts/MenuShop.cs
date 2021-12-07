13c1d5a28422a9f94929a1b4c3d23dacf0cac7bd
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2021-12-07 15:13:29 +0700

    Update bug

--- Assets/Scripts/MenuShop.cs
+++ Assets/Scripts/MenuShop.cs
@@ -31,51 +31,50 @@ public class MenuShop : MonoBehaviour
 
     public void PriceHint(int price)
     {
-        if(coinManager.currentCoin >= coin)
+        switch (price)
         {
-            switch (price)
-            {
-                case 1:
-                    Debug.Log("bayar 1000");
-                    coin = 1000;
-                    hint = 1;
-                    break;
+            case 1:
+                Debug.Log("bayar 1000");
+                coin = 1000;
+                hint = 1;
+                break;
 
-                case 2:
-                    Debug.Log("bayar 2500");
-                    coin = 2500;
-                    hint = 3;
-                    break;
+            case 2:
+                Debug.Log("bayar 2500");
+                coin = 2500;
+                hint = 3;
+                break;
 
-                case 3:
-                    Debug.Log("bayar 7000");
-                    coin = 7000;
-                    hint = 10;
-                    break;
+            case 3:
+                Debug.Log("bayar 7000");
+                coin = 7000;
+                hint = 10;
+                break;
 
-                case 4:
-                    Debug.Log("bayar 8500");
-                    coin = 8500;
-                    hint = 15;
-                    break;
+            case 4:
+                Debug.Log("bayar 8500");
+                coin = 8500;
+                hint = 15;
+                break;
 
-                case 5:
-                    Debug.Log("bayar 10000");
-                    coin = 10000;
-                    hint = 20;
-                    break;
-            }
+            case 5:
+                Debug.Log("bayar 10000");
+                coin = 10000;
+                hint = 20;
+                break;
+        }
+
+        if (coinManager.currentCoin >= coin)
+        {
             coinManager.currentCoin -= coin;
             hintManager.currentHint += hint;
+            PlayerPrefs.SetInt("Coin", coinManager.currentCoin);
+            PlayerPrefs.SetInt("Hint", hintManager.currentHint);
         }
         else
         {
             Debug.Log("You don't have enough money");
         }
-        
-
-        PlayerPrefs.SetInt("Coin", coinManager.currentCoin);
-        PlayerPrefs.SetInt("Hint", hintManager.currentHint);
 
     }
     public void LoadScene(string _SceneName)
