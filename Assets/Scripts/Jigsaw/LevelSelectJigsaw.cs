cc0299f0342d5c275e18e7c7746c089ae3799618
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2021-12-28 16:34:55 +0700

    Update bug

--- Assets/Scripts/Jigsaw/LevelSelectJigsaw.cs
+++ Assets/Scripts/Jigsaw/LevelSelectJigsaw.cs
@@ -15,7 +15,7 @@ public class LevelSelectJigsaw : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
